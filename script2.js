3ae51cdc9920cf2f5f3967cb81736cd28dec1846
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-05-22 18:43:59 -0300

    noticias-upgrade

--- script2.js
+++ script2.js
@@ -1,1 +1,19 @@
-fetch(`https://api.tibiadata.com/v3/news/archive`).then
+
+ let arquivedetails = document.getElementById('arquive-details')
+ 
+
+function getnews(){ 
+    fetch('https://api.tibiadata.com/v3/newsarchive')
+.then(response => response.json())
+.then(data => {
+    recentNews.innerHTML= `
+    
+    <p>Date: ${data.newsarchive.date} </p>
+    <p>Date: ${data.newsarchive.news} </p>
+    
+    `
+    
+    
+})
+
+}
