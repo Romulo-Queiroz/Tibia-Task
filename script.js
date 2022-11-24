b42659b8f2325135e0522c8d1d14365b51791f12
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-11-24 13:00:58 -0300

    correção de bug
    O arquivo js responsável por trazer as informações de characters estava obsoleto (atualizado);
    HTML estava fora de ordem(atualizado);

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 window.addEventListener('DOMContentLoaded', (name) => {
 
-let monsterDetails = document.getElementById('players-details')
+let playersInfo = document.getElementById('show-info');
 
 function getplayer(name){
   fetch(`https://api.tibiadata.com/v3/character/${name}`)
   .then(response => response.json())
   .then(data => {
-    playersDetails.innerHTML = `
+    playersInfo.innerHTML = `
     <p>Name: ${data.characters.character.name}</p>
+    <p>Sex: ${data.characters.character.sex}</p>
+    <p>Vocation: ${data.characters.character.vocation}</p>
+    <p>Level: ${data.characters.character.level}</p>
+    <p>Archivement Points: ${data.characters.character.achievement_points}</p>
+    <p>World: ${data.characters.character.world}</p>
+    <p>Residence: ${data.characters.character.residence}</p>
+    <p>Account Status: ${data.characters.character.account_status}</p>
+    
+
     ` 
   })
 }
@@ -19,8 +28,7 @@ let nome = document.getElementById('txtChar')
 form.addEventListener('submit', (e)=> {
   e.preventDefault()
   let nomePlayer = nome.value 
-  getmonster(nome)
+  getplayer(nomePlayer)
 
 })
-  
 });
