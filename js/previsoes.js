2a971cd9467c61aca86966d14ddda163cacfbe9c
Rômulo Queiroz <rfcontatosvia@gmail.com>
2022-12-03 18:51:01 -0300

    refactor and feat: refatorando e adicionando novas funcionalidades de previsoes

--- js/previsoes.js
+++ js/previsoes.js
@@ -1,21 +1,34 @@
-async function getMonsters() {
-    const {data} = await axios.get('https://api.simacheck.com/server/lore/Ombra')
-    const loyal_panters = data['Loyal Partners']
-    const Midnight = loyal_panters[2]
-    console.log(Midnight)
-    const draptorchance = document.getElementById('Draptor-chance')
-    draptorchance.innerText = `${Midnight.current_prob *100}%`
-
-    const draptor_last_view = document.getElementById('Draptor-Last-view')
-    draptor_last_view.innerText = moment(Midnight.last_view,'YYYY-MM-DD').from(moment().startOf('day'))
-
-    const Status = document.getElementsByClassName('status-monsters')
-   
-    if (Midnight.current_prob == 0) {
-        document.querySelector('.status-monsters').style.backgroundColor = 'red';
-    }
-    else{
-        document.querySelector('.status-monsters').style.backgroundColor = 'green'; 
-    }
+async function getBoss(type) {
+  const { data } = await axios.get(
+    "https://api.simacheck.com/server/lore/Ombra"
+  );
+  return data[type];
 }
-getMonsters();
+
+function drawBossIten({ current_prob, image_url, boss, last_view }) {
+  const color = current_prob === 0 ? "red" : "green";
+  const lastView = moment(last_view, "YYYY-MM-DD").from(
+    moment().startOf("day")
+  );
+  const porcentage = (current_prob * 100).toFixed(2);
+  return `
+     <tr id="details-table">
+        <td class="${color}" title="Quando a cor estiver vermelho a probabilidade é baixa. Com a probailidade alta fica verde">
+        <img src="${image_url}" alt="Draptor"></td>
+        <td>${boss}</td>
+        <td>${porcentage}%</td>
+        <td>${lastView}</td>
+    </tr>`;
+}
+
+async function innerBoss() {
+  const tBody = document.querySelector("#tBody");
+  let tableContent = "";
+  const bosss = await getBoss("Loyal Partners");
+  bosss.forEach((boss) => {
+    tableContent += drawBossIten(boss);
+  });
+  tBody.innerHTML = tableContent;
+}
+
+innerBoss();
