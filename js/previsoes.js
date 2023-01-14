eb1ffba6d08e280e2629f06b7af6dbfb5a2bc2ad
Rômulo Queiroz <rfcontatosvia@gmail.com>
2023-01-14 20:33:01 -0300

    build: previsões para todos os worlds do game implementado

--- js/previsoes.js
+++ js/previsoes.js
@@ -1,14 +1,23 @@
+let world = 'Antica';
+
+let input = document.querySelector('#input');
+let button = document.getElementById('btn');
+button.addEventListener('click', () => {
+  world = input.value;
+  innerBoss();
+});
+
 async function getBoss(type) {
   const { data } = await axios.get(
-    "https://api.simacheck.com/server/lore/Ombra"
+    `https://api.simacheck.com/server/lore/${world}`
   );
   return data[type];
 }
 
 function drawBossIten({ current_prob, image_url, boss, last_view }) {
-  const color = current_prob === 0 ? "red" : "green";
-  const lastView = moment(last_view, "YYYY-MM-DD").from(
-    moment().startOf("day")
+  const color = current_prob === 0 ? 'red' : 'green';
+  const lastView = moment(last_view, 'YYYY-MM-DD').from(
+    moment().startOf('day')
   );
   const porcentage = (current_prob * 100).toFixed(2);
   return `
@@ -22,9 +31,9 @@ function drawBossIten({ current_prob, image_url, boss, last_view }) {
 }
 
 async function innerBoss() {
-  const tBody = document.querySelector("#tBody");
-  let tableContent = "";
-  const bosss = await getBoss("Loyal Partners");
+  const tBody = document.querySelector('#tBody');
+  let tableContent = '';
+  const bosss = await getBoss('Loyal Partners');
   bosss.forEach((boss) => {
     tableContent += drawBossIten(boss);
   });
